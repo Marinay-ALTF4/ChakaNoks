daa721fc7e41049704ababfd65c7346228091162
Mia Loraine <miaalferezesyong@gmail.com>
2025-09-09 14:40:55 +0800

    Update verification_code.php

--- app/Views/auth/verification_code.php
+++ app/Views/auth/verification_code.php
@@ -5,7 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Verify Code</title>
 </head>
-<link rel="stylesheet" href="<?= base_url('css/verification_code.css') ?>">
 
 <body>
 
